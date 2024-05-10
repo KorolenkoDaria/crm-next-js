df7aa8a3a7a3eb665110014d146403623160d5eb
Korolenko Daria <admem75213@gmail.com>
2024-05-10 18:25:06 +0300

    added react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import AddCompanyButton from './components/add-company-button';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
-      <AddCompanyButton />
     </main>
   );
 }
